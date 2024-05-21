1d30ce5114ccc3e5da6fc2f9eb01f3ef16aad472
jack branch <branchjohnchristopher@gmail.com>
2024-05-21 00:19:50 -0500

    added prolog sort of. doesn't work

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,5 +1,35 @@
-require('lspconfig').millet.setup {}
+local lspconfig = require 'lspconfig'
+local util = require 'lspconfig.util'
 
-require('lspconfig').gleam.setup {}
+lspconfig.millet.setup {}
+
+lspconfig.gleam.setup {}
+
+lspconfig.gopls.setup {}
+
+lspconfig.prolog_lsp = {
+  default_config = {
+    cmd = {
+      'swipl',
+      '-g',
+      'use_module(library(lsp_server)).',
+      '-g',
+      'lsp_server:main',
+      '-t',
+      'halt',
+      '--',
+      'stdio',
+    },
+    filetypes = { 'prolog' },
+    root_dir = util.root_pattern 'pack.pl',
+  },
+  docs = {
+    description = [[
+  https://github.com/jamesnvc/prolog_lsp
+
+  Prolog Language Server
+  ]],
+  },
+}
 
 return {}
